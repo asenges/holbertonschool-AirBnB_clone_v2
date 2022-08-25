b3ed38c599d6bd01951e2ba3ede6c7c74f22e52b
Alex <asenges400@gmail.com>
2022-08-25 11:06:50 -0300

    0x03. AirBnB clone - Deploy static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -9,7 +9,7 @@ sudo mkdir -p ~/data/web_static/shared
 sudo mkdir -p ~/data/web_static/releases/test/
 sudo mkdir -p ~/data/web_static/current
 echo -e "<html>\n<head>\n</head>\n<body>\nHolberton School\n</body>\n</html>" | sudo tee -a ~/data/web_static/releases/test/index.html
-sudo ln -sf ~/data/web_static/releases/test/ ~/data/web_static/current
+sudo ln -sf ~/data/web_static/releases/test/index.html ~/data/web_static/current
 sudo chown -R ubuntu:ubuntu ~/data/
 sudo sed -i '/\tserver_name _;/a \\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
 sudo service nginx restart
