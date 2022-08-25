2734b9b7d3c6aa630a6f871f7ca06a568cdb6e61
Alex <asenges400@gmail.com>
2022-08-25 15:46:03 -0300

    0x03. AirBnB clone - Deploy static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 # 0. Prepare your web servers
-apt update
-apt -y install nginx
-mkdir -p /data/web_static/shared
-mkdir -p /data/web_static/releases/test
-echo -e "<html>\n<head>\n</head>\n<body>\nHolberton School\n</body>\n</html>" | tee -a /data/web_static/releases/test/index.html
-ln -sf /data/web_static/releases/test/ /data/web_static/current
-chown -R ubuntu:ubuntu /data/
-chmod -R 755 /data/
-sed -i '/\tserver_name _;/a \\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
-service nginx restart
+sudo apt update
+sudo apt -y install nginx
+sudo mkdir -p /data/web_static/shared
+sudo mkdir -p /data/web_static/releases/test
+sudo echo -e "<html>\n<head>\n</head>\n<body>\nHolberton School\n</body>\n</html>" | sudo tee -a /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data/
+sudo chmod -R 755 /data/
+sudo sed -i '/\tserver_name _;/a \\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
+sudo service nginx restart
