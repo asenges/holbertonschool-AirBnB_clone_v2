a7bc404720c440b88eb64877c4754bab2f99d4fb
Alex <asenges400@gmail.com>
2022-09-03 08:20:24 -0300

    @asenges 2022-09-03 08:20:24

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python3
+"""
+Server routes
+"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """
+    Hello HBNB!
+    """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """
+    HBNB
+    """
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def cisfun(text):
+    """
+    C Endpoint
+    """
+    return 'C ' + text.replace('_', ' ')
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def pythoniscool(text='is cool'):
+    """
+    Python Endpoint
+    """
+    return 'Python ' + text.replace('_', ' ')
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def isnumber(n):
+    """
+    Integer Endpoint
+    """
+    return "{:d} is a number".format(n)
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def isnumberopentemplate(n):
+    """
+    If Integer displays HTML
+    """
+    return render_template('5-number.html', n=n)
+
+
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+def isoddevennumberopentemplate(n):
+    """
+    If Integer displays HTML
+    """
+    if n % 2 == 0:
+        evenodd = 'even'
+    else:
+        evenodd = 'odd'
+    return render_template('6-number_odd_or_even.html', n=n,
+                           evenness=evenness)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
